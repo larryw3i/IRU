b0331565c1a8413220ff5782021dc3818a0cad17
larryw3i <larryw3i@163.com>
2020-06-05 15:36:29 +0800

    optimize UI

--- iru.py
+++ iru.py
@@ -1,10 +1,12 @@
 
+import os
 import locale
 import gettext
 from interfaces import mainui
 
 default_locale  = locale.getdefaultlocale()[0]
-default_locale = default_locale if len( default_locale ) > 0 else 'en_US'
+
+if default_locale not in  os.listdir('locale'): default_locale = 'en_US'
 
 _ = gettext.translation('iru', 'locale', [ default_locale ] )\
     .gettext
