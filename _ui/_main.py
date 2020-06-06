d8105553af229a73a81ff5512e8baef784278e68
larryw3i <larryw3i@163.com>
2020-06-06 21:24:13 +0800

    add get & save iru_persons

--- _ui/_main.py
+++ _ui/_main.py
@@ -1,10 +1,15 @@
 
-
+import os
+import pickle
+from tkinter import messagebox
 from ui.face_entry_ui import face_entry
 from ui.face_recg_ui import face_recg
 
 class MainUI:
-    def __init__(self):
+    def __init__(
+        self,
+        window: tkinter.Tk  ):
+        self.faces_path = './data/iru_person.iru'
         pass
 
     def to_face_recg(self):
@@ -14,4 +19,14 @@ class MainUI:
         face_entry()
     
     def get_iru_persons( self ):
-        
+        if os.path.exists( self.faces_path ):
+
+            with open( self.faces_path , 'rb' ) as f:
+                return pickle.load( f )
+
+        else: 
+            messagebox.showinfo( message = 'Nothing was read' )
+    
+    def save_iru_persons( self , iru_persons ):
+        with open( self.faces_path , 'wb' ) as f:
+            pickle.dump( iru_persons, f )
